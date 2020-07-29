0b38cae7c95dd1a0f5e069c501ece7e9788c1d4f
unknown <boulafaacharaf@gmail.com>
2020-07-29 03:26:25 -0400

    hero responsive

--- src/components/hero.js
+++ src/components/hero.js
@@ -15,15 +15,15 @@ export default function Hero() {
         }
     `)
     return (
-        <div className="flex items-center pt-24 pb-12">
-            <div className="w-2/4">
+        <div className="flex flex-wrap items-center pt-24 pb-12">
+            <div className="w-full text-center sm:w-2/4 sm:text-left">
                 <h1 className="text-4xl font-extrabold">Charaf Boulafaa</h1>
                 <p className="text-xl font-semibold my-4">A Software Engineer</p>
                 <p>Focused on giving efficient and creative results, and determined to be a integral part of a development team.</p>
                 <button className="bg-blue-500 hover:bg-blue-600 text-white py-1 px-4 rounded mt-4 tracking-wide">See Projects</button>
             </div>
 
-            <div className="w-2/4">
+            <div className="hidden sm:block sm:w-2/4">
                 <Img fluid={data.codingImage.childImageSharp.fluid} />
             </div>
         </div>
