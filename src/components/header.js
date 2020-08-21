9d562ebce93b206415b53642e303fefd45e70cfd
unknown <boulafaacharaf@gmail.com>
2020-08-20 22:36:38 -0400

    heroku setup

--- src/components/header.js
+++ src/components/header.js
@@ -6,15 +6,15 @@ import scrollTo from 'gatsby-plugin-smoothscroll';
 const Header = ({ siteTitle }) => (
   <header className="py-3 flex justify-between">
     <div>
-      <h1 className="font-bold">
+      {/* <h1 className="font-bold">
         <Link to="/">{siteTitle}</Link>
-      </h1>
+      </h1> */}
     </div>
 
     <div className="flex">
-      <button onClick={()=> scrollTo('#about')} className="mx-1">About</button>
-      <button onClick={()=> scrollTo('#portfolio')} className="mx-1">Portfolio</button>
-      <button onClick={()=> scrollTo('#contact')} className="mx-1">Contact</button>
+      <button onClick={()=> scrollTo('#about')} className="mx-2">About</button>
+      <button onClick={()=> scrollTo('#portfolio')} className="mx-2">Portfolio</button>
+      <button onClick={()=> scrollTo('#contact')} className="mx-2">Contact</button>
     </div>
   </header>
 )
